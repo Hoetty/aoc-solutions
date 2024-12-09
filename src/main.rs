63eea42247d1793bc2880b817624c60cf70b88c8
Hoetty <kilian.behnke@outlook.com>
2024-12-09 21:40:36 +0100

    Timing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::time::Instant;
+
 pub mod day1;
 pub mod day2;
 pub mod day3;
@@ -8,14 +10,24 @@ pub mod day7;
 pub mod day8;
 pub mod day9;
 
+pub fn time(name: &str ,function: &dyn Fn()) {
+    let start = Instant::now();
+    function();
+    println!("{}: {}ms", name, start.elapsed().as_micros() as f64 / 1000.0);
+}
+
+fn solve_all() {
+    time("Day 1", &day1::solutions);
+    time("Day 2", &day2::solutions);
+    time("Day 3", &day3::solutions);
+    time("Day 4", &day4::solutions);
+    time("Day 5", &day5::solutions);
+    time("Day 6", &day6::solutions);
+    time("Day 7", &day7::solutions);
+    time("Day 8", &day8::solutions);
+    time("Day 9", &day9::solutions);
+}
+
 fn main() {
-    day1::solutions();
-    day2::solutions();
-    day3::solutions();
-    day4::solutions();
-    day5::solutions();
-    day6::solutions();
-    day7::solutions();
-    day8::solutions();
-    day9::solutions();
+    time("Total", &solve_all);
 }
