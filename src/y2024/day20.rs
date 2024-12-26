e03e6b4f056d8adfdb7cd553affd75c887179712
Hoetty <kilian.behnke@outlook.com>
2024-12-26 21:18:39 +0100

    Try Optimize Day 20

--- src/y2024/day20.rs
+++ src/y2024/day20.rs
@@ -45,7 +45,7 @@ fn get_input(file: &str) -> Maze {
 const MIN_SKIP_DISTANCE: u32 = 100;
 
 fn solve_first(input: Maze) -> usize {
-    let mut tiles: Vec<u32> = input.tiles.iter().map(|b| if *b { u32::MAX - 1 } else { u32::MAX }).collect();
+    let mut tiles: Vec<u16> = input.tiles.iter().map(|b| if *b { u16::MAX - 1 } else { u16::MAX }).collect();
     let area = tiles.len();
     let floor = input.width * 2;
     let ceiling = area - floor;
@@ -81,13 +81,13 @@ fn solve_first(input: Maze) -> usize {
 
         distance += 1;
 
-        if tiles[pos + 1] == u32::MAX {
+        if tiles[pos + 1] == u16::MAX {
             pos += 1;
-        } else if tiles[pos - 1] == u32::MAX {
+        } else if tiles[pos - 1] == u16::MAX {
             pos -= 1;
-        } else if tiles[pos + input.width] == u32::MAX {
+        } else if tiles[pos + input.width] == u16::MAX {
             pos += input.width;
-        } else if tiles[pos - input.width] == u32::MAX {
+        } else if tiles[pos - input.width] == u16::MAX {
             pos -= input.width;
         } else {
             panic!();
@@ -97,68 +97,91 @@ fn solve_first(input: Maze) -> usize {
     sub_hundred
 }
 
-const CHEAT_DISTANCE: isize = 20;
+const CHEAT_DISTANCE: u8 = 20;
 
 fn solve_second(input: Maze) -> usize {
-    let mut tiles: Vec<u32> = input.tiles.iter().map(|b| if *b { u32::MAX - 1 } else { u32::MAX }).collect();
+    let mut tiles: Vec<u16> = input.tiles.iter().map(|b| if *b { 0 } else { u16::MAX }).collect();
     let area = tiles.len();
-    let height = area / input.width;
 
-    let mut distance = 0;
-    let mut sub_hundred = 0;
     let mut pos = input.end;
 
+    let mut path: Vec<(u8, u8)> = Vec::with_capacity(area / 4);
+    let mut chunks: Vec<Vec<((u8, u8), usize)>> = vec![vec![]; 49];
+
+    let mut distance_from_end = 0;
+
     loop {
-        tiles[pos] = distance;
-        
-        {
-            let middle_x = pos % input.width;
-            let middle_y = pos / input.width;
+        tiles[pos] = 0;
 
-            let min_dx = (-(middle_x as isize)).max(-CHEAT_DISTANCE);
-            let min_dy = (-(middle_y as isize)).max(-CHEAT_DISTANCE);
+        let x = pos % input.width;
+        let y = pos / input.width;
 
-            let max_dx = (input.width - middle_x - 1).min(CHEAT_DISTANCE as usize) as isize;
-            let max_dy = (height - middle_y - 1).min(CHEAT_DISTANCE as usize) as isize;
-
-            for x in min_dx..=max_dx {
-                let x_abs = x.abs();
-                let x_pos = (pos as isize + x) as usize;
-
-                let min_dy = (-CHEAT_DISTANCE + x_abs).max(min_dy);
-                let max_dy = (CHEAT_DISTANCE - x_abs).min(max_dy);
-
-                for y in min_dy..=max_dy {
-                    let y_abs = y.abs();
-                    let final_pos = (x_pos as isize + (input.width as isize * y)) as usize;
-
-                    let length = x_abs + y_abs;
-
-                    if distance >= (MIN_SKIP_DISTANCE + length as u32) && tiles[final_pos] <= distance - (MIN_SKIP_DISTANCE + length as u32) {
-                        sub_hundred += 1;
-                    }
-                }
-            }
-        }
+        chunks[x / 20 + y / 20 * 7].push(((x as u8, y as u8), distance_from_end));
+        path.push((x as u8, y as u8));
+        distance_from_end += 1;
 
         if pos == input.start {
             break;
         }
 
-        distance += 1;
-
-        if tiles[pos + 1] == u32::MAX {
+        if tiles[pos + 1] == u16::MAX {
             pos += 1;
-        } else if tiles[pos - 1] == u32::MAX {
+        } else if tiles[pos - 1] == u16::MAX {
             pos -= 1;
-        } else if tiles[pos + input.width] == u32::MAX {
+        } else if tiles[pos + input.width] == u16::MAX {
             pos += input.width;
-        } else if tiles[pos - input.width] == u32::MAX {
+        } else if tiles[pos - input.width] == u16::MAX {
             pos -= input.width;
         } else {
             panic!();
         }
     }
+
+    let last_possible_index = path.len() - MIN_SKIP_DISTANCE as usize;
+    let mut sub_hundred = 0;
+
+    for (lower_index, lower_coords) in path.iter().enumerate() {
+        if lower_index >= last_possible_index {
+            break;
+        }
+
+        let middle_chunk = lower_coords.0 / 20 + lower_coords.1 / 20 * 7;
+        let middle_x = middle_chunk % 7;
+        let middle_y = middle_chunk / 7;
+
+        let min_dx = if middle_x > 0 { -1 } else { 0 };
+        let max_dx = if middle_x < 6 { 1 } else { 0 };
+        let min_dy = if middle_y > 0 { -1 } else { 0 };
+        let max_dy = if middle_y < 6 { 1 } else { 0 };
+
+        for chunk_dx in min_dx..=max_dx {
+            for chunk_dy in min_dy..max_dy {
+                for (higher_coords, higher_index) in &chunks[(middle_chunk as isize + chunk_dx + 7 * chunk_dy) as usize] {
+                    let distance = lower_coords.0.abs_diff(higher_coords.0) as usize + lower_coords.1.abs_diff(higher_coords.1) as usize;
+
+                    if distance <= CHEAT_DISTANCE as usize && *higher_index >= distance + lower_index + MIN_SKIP_DISTANCE as usize {
+                        sub_hundred += 1;
+                    }
+                }
+            }
+        }
+    }
+
+
+
+    // for (lower_index, lower_coords) in path.iter().enumerate() {
+    //     if lower_index >= last_possible_index {
+    //         break;
+    //     }
+
+    //     for (higher_index, higher_coords) in path[lower_index + MIN_SKIP_DISTANCE as usize..].iter().enumerate() {
+    //         let distance = lower_coords.0.abs_diff(higher_coords.0) as usize + lower_coords.1.abs_diff(higher_coords.1) as usize;
+
+    //         if distance <= CHEAT_DISTANCE as usize && higher_index >= distance {
+    //             sub_hundred += 1;
+    //         }
+    //     }
+    // }
 
     sub_hundred
 }
