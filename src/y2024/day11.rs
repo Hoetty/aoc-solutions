e8c47a80d88f81d2c286c331f125524acd1c1d90
Hoetty <kilian.behnke@outlook.com>
2024-12-30 14:20:30 +0100

    Update Day 11

--- src/y2024/day11.rs
+++ src/y2024/day11.rs
@@ -1,6 +1,6 @@
 use std::fs;
 
-use rustc_hash::FxHashMap;
+use rustc_hash::{FxBuildHasher, FxHashMap};
 
 use crate::solutions;
 
@@ -9,85 +9,106 @@ solutions!{2024, 11}
 fn get_input(file: &str) -> FxHashMap<u64, usize> {
     let input: Vec<u64> = fs::read_to_string(file).expect("No file there").split_whitespace().map(|s| s.parse().unwrap()).collect();
 
-    let mut stones = stones_to_map(&input);
-    mutate_n_times(&mut stones, 25);
-
-    stones
+    // Precalculate 25 Mutations to use later
+    let stones = stones_to_map(&input);
+    mutate_n_times::<25>(&stones)
 }
 
+/// Tests if a number has an even amount of digits, and splits it into two
+/// 123 -> None
+/// 1234 -> Some((12, 34))
 fn is_splittable(num: u64) -> Option<(u64, u64)> {
-    let mut i = 0;
-    let mut t = 1;
-    let mut t_half = 1;
+    let mut is_odd = 0;
+    let mut ten = 1;
+    let mut ten_half = 1;
 
-    while t <= num {
-        i += 1;
-        t *= 10;
-        if i & 1 == 0 {
-            t_half *= 10;
+    while ten <= num {
+        is_odd += 1;
+        ten *= 10;
+        if is_odd & 1 == 0 {
+            ten_half *= 10;
         }
     }
 
-    if i & 1 == 1 {
+    if is_odd & 1 == 1 {
         return None;
     }
 
-    let right = num % t_half;
-    let left = (num - right) / t_half;
+    let right = num % ten_half;
+    let left = (num - right) / ten_half;
     Some((left, right))
 }
 
+/// Stores the result of what a stone becomes after blinking
 enum Mutation {
     One(u64),
     Two(u64, u64)
 }
 
-fn mutate(num: u64) -> Mutation {
-    if num == 0 {
+/// Mutate a stone once
+/// If its 0 then it becomes 1
+/// Else if its splittable, left and right are returned
+/// Else the number is multiplied by 2024
+fn mutate(stone: u64) -> Mutation {
+    if stone == 0 {
         Mutation::One(1)
-    } else if let Some((left, right)) = is_splittable(num) {
+    } else if let Some((left, right)) = is_splittable(stone) {
         Mutation::Two(left, right)
     } else {
-        Mutation::One(num * 2024)
+        Mutation::One(stone * 2024)
     }
 }
 
-fn mutate_all(stones: &mut FxHashMap<u64, usize>) {
-    let last_stones: Vec<_> = stones.drain().collect();
+/// Mutates all stones in the HashMap. And returns a new HashMap with the mutated stone counts
+fn mutate_all(stones: &FxHashMap<u64, usize>) -> FxHashMap<u64, usize> {
+    let mut next_stones = FxHashMap::with_capacity_and_hasher(stones.len() + stones.len() / 2, FxBuildHasher);
 
-    for (stone, count) in last_stones {
-        match mutate(stone) {
-            Mutation::One(new_stone) => *stones.entry(new_stone).or_default() += count,
+    for (stone, count) in stones {
+        match mutate(*stone) {
+            Mutation::One(new_stone) => *next_stones.entry(new_stone).or_default() += count,
             Mutation::Two(first_stone, second_stone) => {
-                *stones.entry(first_stone).or_default() += count;
-                *stones.entry(second_stone).or_default() += count;
+                *next_stones.entry(first_stone).or_default() += count;
+                *next_stones.entry(second_stone).or_default() += count;
             },
         }
     }
+
+    next_stones
 }
 
+/// Converts a list of stones into a count map, 
+///    where the key is the stones number and the value how often it appeared in the list
 fn stones_to_map(stones: &[u64]) -> FxHashMap<u64, usize> {
-    let mut blink_stones: FxHashMap<u64, usize> = FxHashMap::default();
+    let mut stone_counts: FxHashMap<u64, usize> = FxHashMap::default();
 
     for stone in stones {
-        *blink_stones.entry(*stone).or_default() += 1;
+        *stone_counts.entry(*stone).or_default() += 1;
     }
 
-    blink_stones
+    stone_counts
 }
 
-fn mutate_n_times(stones: &mut FxHashMap<u64, usize>, n: usize) {
-    for _ in 0..n {
-        mutate_all(stones);
+/// Mutates the stones n times and returns the new HashMap
+fn mutate_n_times<const N: usize>(stones: &FxHashMap<u64, usize>) -> FxHashMap<u64, usize> {
+    let mut new_stones = mutate_all(stones);
+    for _ in 0..N - 1 {
+        new_stones = mutate_all(&new_stones);
     }
+
+    new_stones
 }
 
+/// ### 25 Blinks
+/// 
+/// Count the number of stones after 25 blinks
 fn solve_first(input: &FxHashMap<u64, usize>) -> usize {
     input.values().sum()
 }
 
+/// ### 75 Blinks
+/// 
+/// Count the number of stones after 75 blinks
 fn solve_second(input: &FxHashMap<u64, usize>) -> usize {
-    let mut stones = input.clone();
-    mutate_n_times(&mut stones, 50);
+    let stones = mutate_n_times::<50>(input);
     stones.values().sum()
 }
