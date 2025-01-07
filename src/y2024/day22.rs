d1a0a2e63158351af59efe3fec00cb82f7e01549
Hoetty <kilian.behnke@outlook.com>
2025-01-07 21:54:15 +0100

    Combine Day 22 Part 1 and 2

--- src/y2024/day22.rs
+++ src/y2024/day22.rs
@@ -4,10 +4,6 @@ use crate::solutions;
 
 solutions!{2024, 22}
 
-fn get_input(file: &str) -> Vec<u32> {
-    fs::read_to_string(file).expect("No file there").lines().map(|line| line.parse().unwrap()).collect()
-}
-
 #[inline(always)]
 fn mix(lhs: u32, rhs: u32) -> u32 {
     lhs ^ rhs
@@ -19,34 +15,17 @@ fn prune(lhs: u32) -> u32 {
 }
 
 #[inline]
-fn step_unpruned(secret: u32) -> u32 {
+fn step(secret: u32) -> u32 {
     let step1 = prune(mix(secret << 6, secret));
     let step2 = mix(step1 >> 5, step1);
-    mix(step2 << 11, step2)
+    prune(mix(step2 << 11, step2))
 }
 
-#[inline]
-fn step(secret: u32) -> u32 {
-    prune(step_unpruned(secret))
-}
+fn get_input(file: &str) -> (u64, u16) {
+    let input: Vec<_> = fs::read_to_string(file).expect("No file there").lines().map(|line| line.parse().unwrap()).collect();
 
-fn step_n_times(secret: u32, n: usize) -> u32 {
-    let mut secret = secret;
+    let mut two_thousands = 0;
 
-    for _ in 0..n {
-        secret = step_unpruned(secret);
-    }
-
-    prune(secret)
-}
-
-fn solve_first(input: &[u32]) -> u64 {
-    input.iter().map(|num| step_n_times(*num, 2000) as u64).sum()
-}
-
-const MASK: usize = (1 << 20) - 1;
-
-fn solve_second(input: &[u32]) -> u16 {
     let mut scores: Vec<u16> = vec![0; 1 << 20];
     let mut seen: Vec<u16> = vec![0; 1 << 20];
 
@@ -57,8 +36,8 @@ fn solve_second(input: &[u32]) -> u16 {
         for i in 0..2000 {
             let next = step(secret);
     
-            let bananas = (next as i32 % 10) as i8;
-            let bananas_last = (secret as i32 % 10) as i8;
+            let bananas = (next % 10) as i8;
+            let bananas_last = (secret % 10) as i8;
     
             sequence = (sequence << 5) | (10 + bananas - bananas_last) as usize;
     
@@ -69,7 +48,19 @@ fn solve_second(input: &[u32]) -> u16 {
                 seen[sequence & MASK] = monkey as u16 + 1;
             }
         }
+
+        two_thousands += secret as u64;
     }
 
-    *scores.iter().max().unwrap()
+    (two_thousands, *scores.iter().max().unwrap())
+}
+
+fn solve_first(input: &(u64, u16)) -> u64 {
+    input.0
+}
+
+const MASK: usize = (1 << 20) - 1;
+
+fn solve_second(input: &(u64, u16)) -> u16 {
+    input.1
 }
